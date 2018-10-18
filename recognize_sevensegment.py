611f65685c9799dba7b9af13ef8fc7bf17d33d71
RASYADH\Rasyadh A Aziz <rasyadhabdulaziz@gmail.com>
2018-10-18 14:36:02 +0700

    seven segment recognition init

---
+++ recognize_sevensegment.py
@@ -0,0 +1,192 @@
+from imutils.perspective import four_point_transform
+from imutils import contours
+import imutils
+import cv2
+import numpy as np
+
+# Define the dictionary of digit 7-segment
+DIGITS_LOOKUP = {
+    #(0, 1, 2, 3, 4, 5, 6)
+    (1, 1, 1, 0, 1, 1, 1): 0,
+    (0, 0, 1, 0, 0, 1, 0): 1,
+    (1, 0, 1, 1, 1, 0, 1): 2,
+    (1, 0, 1, 1, 0, 1, 1): 3,
+    (0, 1, 1, 1, 0, 1, 0): 4,
+    (1, 1, 0, 1, 0, 1, 1): 5,
+    (1, 1, 0, 1, 1, 1, 1): 6,
+    (1, 1, 1, 0, 0, 1, 0): 7,
+    (1, 1, 1, 1, 1, 1, 1): 8,
+    (1, 1, 1, 1, 0, 1, 1): 9
+}
+
+def load_image(path):    
+    # load the image
+    image = cv2.imread(path)
+    # print('image shape: {}'.format(image.shape))
+    return image
+
+def preprocessing(image, height=100, kernel=(5, 5)):
+    # pre-processing the image
+    # resizing, grayscaling, blurring, and edge map
+    image = imutils.resize(image, height=height)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, kernel, 0)
+    # print('image shape resized: {}'.format(image.shape))
+    return image, blurred
+
+def get_threshold(blurred):
+    # threshold image
+    threshold = cv2.threshold(
+        blurred, 0, 255, 
+        cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU
+    )[1]
+    # apply morphological operator
+    kernel = cv2.getStructuringElement(
+        cv2.MORPH_ELLIPSE, (1, 5)
+    )
+    # kernel = cv2.getStructuringElement(
+    #     cv2.MORPH_CROSS, (5, 5)
+    # )
+    threshold = cv2.morphologyEx(
+        threshold, 
+        cv2.MORPH_OPEN, 
+        kernel
+    )
+    # print('kernel: {}'.format(kernel))
+    return threshold
+
+def get_adaptive_threshold(blurred):
+    # apply CLAHE
+    # (Contrast Limited Adaptive Histogram Equalization)
+    clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(6, 6))
+    blurred = clahe.apply(blurred)
+    # apply adaptive threshold
+    threshold = cv2.adaptiveThreshold(
+        blurred, 255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,
+        127, 10)
+
+    # get structuring element
+    # kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+    # perform morphological transformation
+    threshold = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
+    return threshold
+
+def find_digits(threshold, image):
+    #find contours in the threshold image
+    cnts = cv2.findContours(
+        threshold.copy(),
+        cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE
+    )
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    digitsCnts = []
+    # print('cnts: {}'.format(cnts))
+
+    # loop over the digit area candidates
+    for c in cnts:
+        # bounding boxx
+        (x, y, w, h) = cv2.boundingRect(c)
+        print('x, y, w, h: {}'.format((x, y, w, h)))
+
+        # get the digit of the contours
+        if w >= 10 and (h >= 60 and h <= 90):
+            digitsCnts.append(c)
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 1)
+        '''
+        if h >= 70 and h <= 90:
+            if w <= 20:
+                digitsCnts.append(c)
+                cv2.rectangle(
+                    image, (x, y), (x+w, y+h), (0, 255, 0), 1
+                )
+            elif w >= 30 and w <= 60:
+                digitsCnts.append(c)
+                cv2.rectangle(
+                    image, (x, y), (x+w, y+h), (0, 255, 0), 1
+                )
+        '''
+    print('digits contours detected : {}'.format(len(digitsCnts)))
+    
+    # if digits not found throw assertion error
+    assert len(digitsCnts) > 0, "Failed to find digit's position"
+
+    # sort the contours from left-to-right
+    digitsCnts = contours.sort_contours(
+        digitsCnts,
+        method="left-to-right"
+    )[0]
+    return digitsCnts
+
+def recognize_digits(image, threshold, digitsCnts):
+    digits = []
+    # loop over each of the digits
+    for c in digitsCnts:
+        # extract the digit ROI
+        (x, y, w, h) = cv2.boundingRect(c)
+        # get region of interest (roi) from threshold image
+        roi = threshold[y : y+h, x : x+w]
+        # print('roi: {}'.format(roi))
+
+        # compute the width and height of each the 7 segments
+        (roiH, roiW) = roi.shape
+        (dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
+        dHC = int(roiH * 0.05)
+        print('roi shape: {}'.format(roi.shape))
+        print('dw, dh: {}'.format((dW, dH)))
+        print('dHC: {}'.format(dHC))
+
+        # define the set of 7 segments
+        segments = [
+            ((0, 0), (w, dH)),          # top
+            ((0, 0), (dW, h // 2)),     # top-left
+            ((w - dW, 0), (w, h // 2)), # top-right
+            ((0, (h // 2) - dHC), (w, (h // 2) + dHC)),  # center
+            ((0, h // 2), (dW, h)),     # bottom-left
+            ((w - dW, h // 2), (w, h)), # bottom-right
+            ((0, h -dH), (w, h))        # bottom
+        ]
+        on = [0] * len(segments)
+
+        # loop over the segments
+        for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
+            # extract the segment ROI
+            segROI = roi[yA:yB, xA:xB]
+            # count the total of thresholded pixel in the segment
+            total = cv2.countNonZero(segROI)
+            area = (xB - xA) * (yB - yA)
+
+            # if total number of non-zero pixel is greater 
+            # than 50% of the area, mark the segment as on
+            if total / float(area) > 0.4:
+                on[i] = 1
+
+        # lookup the digit
+        if tuple(on) in DIGITS_LOOKUP.keys():
+            digit = DIGITS_LOOKUP[tuple(on)]
+            digits.append(digit)
+            # draw roi on the image
+            cv2.rectangle(
+                image, (x, y), (x+w, y+h), (0, 255, 0), 1
+            )
+            cv2.putText(
+                image, str(digit), (x, y+15),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                (0, 255, 0), 1
+            )
+    return digits
+
+if __name__ == '__main__':
+    image = load_image('test/4840.png')
+    image, blurred = preprocessing(image, kernel=(7, 7))
+    threshold = get_threshold(blurred)
+    #threshold = get_adaptive_threshold(blurred)
+    digitsCnts = find_digits(threshold, image)
+    digits = recognize_digits(image, threshold, digitsCnts)
+
+    # print('Digits : {}'.format(digits))
+    cv2.imshow('threshold', threshold)
+    cv2.imshow('result', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
---
+++ sevensegment_recog.py
@@ -0,0 +1,182 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+DIGITS_LOOKUP = {
+    (1, 1, 1, 1, 1, 1, 0): 0,
+    (1, 1, 0, 0, 0, 0, 0): 1,
+    (1, 0, 1, 1, 0, 1, 1): 2,
+    (1, 1, 1, 0, 0, 1, 1): 3,
+    (1, 1, 0, 0, 1, 0, 1): 4,
+    (0, 1, 1, 0, 1, 1, 1): 5,
+    (0, 1, 1, 1, 1, 1, 1): 6,
+    (1, 1, 0, 0, 0, 1, 0): 7,
+    (1, 1, 1, 1, 1, 1, 1): 8,
+    (1, 1, 1, 0, 1, 1, 1): 9
+}
+
+H_W_RATIO = 1.9
+THRESHOLD = 35
+arc_tan_theta = 6.0
+
+def load_image(path):
+    # load image as grayscale
+    gray = cv2.imread(path, 0)
+    # get width and height of the image
+    height, width = gray.shape
+    # apply gaussian blur to gray image
+    blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+    # show gray and blurred image
+    cv2.imshow('gray', gray)
+    cv2.imshow('blurred', blurred)
+    return blurred, gray
+
+def preprocess(image, threshold, kernel_size=(5, 5)):
+    # apply CLAHE 
+    # (Contrast Limited Adaptive Histogram Equalization)
+    clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(6, 6))
+    image = clahe.apply(image)
+    cv2.imshow('clahe', image)
+    # apply adaptive threshold 
+    # to get black and white image
+    dst = cv2.adaptiveThreshold(
+        image, 255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,
+        127, threshold)
+    cv2.imshow('adaptive threshold', dst)
+    # get structuring element
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, kernel_size)
+    # perform morphological transformation
+    dst = cv2.morphologyEx(dst, cv2.MORPH_CLOSE, kernel)
+    cv2.imshow('morph close', dst)
+    dst = cv2.morphologyEx(dst, cv2.MORPH_OPEN, kernel)
+    cv2.imshow('morph open', dst)
+    return dst
+
+def helper_extract(one_d_array, threshold=20):
+    res = []
+    flag = 0
+    temp = 0
+
+    for i in range(len(one_d_array)):
+        
+        if one_d_array[i] < 12 * 255:
+            if flag > threshold:
+                start = i - flag
+                end = i
+                temp = end
+                if end - start > 20:
+                    res.append((start, end))
+            flag = 0
+        else:
+            flag += 1
+    else:
+        if flag > threshold:
+            start = temp
+            end = len(one_d_array)
+            if end - start > 50:
+                res.append((start, end))
+    return res
+
+def find_digits_positions(image, reserved_threshold=20):
+    digits_positions = []
+    # calculate horizontal position
+    img_array = np.sum(image, axis=0)
+    horizontal_position = helper_extract(img_array, threshold=reserved_threshold)
+    # calculate vertical position
+    img_array = np.sum(image, axis=1)
+    vertical_position = helper_extract(img_array, threshold=reserved_threshold * 4)
+    # make vertical position has only one element
+    if len(vertical_position) > 1:
+        vertical_position = [
+            (vertical_position[0][0], vertical_position[len(vertical_position) - 1][1])
+        ]
+    print('horizontal position : {}'.format(horizontal_position))
+    print('vertical position : {}'.format(vertical_position))
+    # create coordinat from horizontal and vertical positions
+    for horizontal in horizontal_position:
+        for vertical in vertical_position:
+            digits_positions.append(list(zip(horizontal, vertical)))
+    # check if digits's found
+    # if not throw assertion error
+    assert len(digits_positions) > 0, "Failed to find digits's positions"
+    return digits_positions
+
+def recognize_digits_line_method(digits_positions, output_img, input_img):
+    digits = []
+    for coordinat in digits_positions:
+        x0, y0 = coordinat[0]
+        x1, y1 = coordinat[1]
+        roi = input_img[y0:y1, x0:x1]
+        h, w = roi.shape
+        suppose_W = max(1, int(h / H_W_RATIO))
+
+        if x1 - x0 < 25 and cv2.countNonZero(roi) / ((y1 - y0) * (x1 - x0)) < 0.2:
+            continue
+
+        if w < suppose_W / 2:
+            x0 = max(x0 + w - suppose_W, 0)
+            roi = input_img[y0:y1, x0:x1]
+            w = roi.shape[1]
+
+        center_y = h // 2
+        quater_y_1 = h // 4
+        quater_y_3 = quater_y_1 * 3
+        center_x = w // 2
+        line_width = 5  # line's width
+
+        width = (max(int(w * 0.15), 1) + max(int(h * 0.15), 1)) // 2
+        small_delta = int(h / arc_tan_theta) // 4
+        segments = [
+            ((w - 2 * width, quater_y_1 - line_width), (w, quater_y_1 + line_width)),
+            ((w - 2 * width, quater_y_3 - line_width), (w, quater_y_3 + line_width)),
+            ((center_x - line_width - small_delta, h - 2 * width), (center_x - small_delta + line_width, h)),
+            ((0, quater_y_3 - line_width), (2 * width, quater_y_3 + line_width)),
+            ((0, quater_y_1 - line_width), (2 * width, quater_y_1 + line_width)),
+            ((center_x - line_width, 0), (center_x + line_width, 2 * width)),
+            ((center_x - line_width, center_y - line_width), (center_x + line_width, center_y + line_width)),
+        ]
+        on = [0] * len(segments)
+
+        for (i, ((xa, ya), (xb, yb))) in enumerate(segments):
+            seg_roi = roi[ya:yb, xa:xb]
+            total = cv2.countNonZero(seg_roi)
+            area = (xb - xa) * (yb - ya) * 0.9
+
+            if total / float(area) > 0.25:
+                on[i] = 1
+        
+        if tuple(on) in DIGITS_LOOKUP.keys():
+            digit = DIGITS_LOOKUP[tuple(on)]
+        else:
+            digit = ''
+        digits.append(digit)
+
+        # detect dot in seven segment
+        '''
+        if cv2.countNonZero(roi[h - int(3 * width / 4):h, w - int(3 * width / 4):w]) / (9. / 16 * width * width) > 0.65:
+            digits.append('.')
+            cv2.rectangle(output_img,
+                          (x0 + w - int(3 * width / 4), y0 + h - int(3 * width / 4)),
+                          (x1, y1), (0, 128, 0), 2)
+            cv2.putText(output_img, 'dot',
+                        (x0 + w - int(3 * width / 4), y0 + h - int(3 * width / 4) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 128, 0), 2)
+        '''
+
+        cv2.rectangle(output_img, (x0, y0), (x1, y1), (0, 128, 0), 2)
+        cv2.putText(output_img, str(digit), (x0 + 3, y0 + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 128, 0), 2)
+    return digits
+
+if __name__ == '__main__':
+    blurred, gray = load_image('test/39A99.jpg')
+    print(blurred.shape)
+    output = blurred
+    dst = preprocess(blurred, THRESHOLD)
+    digits_positions = find_digits_positions(dst)
+    print('digits position : {}'.format(digits_positions))
+    digits = recognize_digits_line_method(digits_positions, output, dst)
+    print('digits : {}'.format(digits))
+    cv2.imshow('result', output)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
